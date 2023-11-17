2123d798f10fe4b763c535e819540a89b733a728
cruizwell63 <cruizwell63@gmail.com>
2023-11-17 18:24:53 +0800

    Add project files.

---
+++ FaceRecognition Practice/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace FaceRecognition_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FaceRec rc = new FaceRec();
+            rc.openCamera(pictureBox1,pictureBox2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FaceRec rc = new FaceRec();
+
+            if (textBox1.Text != "")
+            {
+                rc.Save_IMAGE(textBox1.Text);
+                db.isSave(textBox1.Text);
+                MessageBox.Show("Saved Successfully", "Saved");
+
+            }
+            else {
+                MessageBox.Show("Please fill name");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FaceRec rc = new FaceRec();
+            rc.isTrained = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            this.Hide();
+            f2.Show();
+        }
+    }
+}
---
+++ FaceRecognition Practice/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FaceRecognition;
+
+namespace FaceRecognition_Practice
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        FaceRec rc = new FaceRec();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            rc.isTrained = true;
+            label1.Hide();
+            rc.getPersonName(label1);
+            pictureBox2.Hide();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (db.present(label1.Text) == 1)
+            {
+                MessageBox.Show("Logged in successfully");
+            }
+            else
+                MessageBox.Show("Log in failed.");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rc.openCamera(pictureBox1, pictureBox2);
+        }
+    }
+}
